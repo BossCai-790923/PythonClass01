dd829156da5894096db6a432acacdb3f42c890f1
Tom Fan <tomboxfan@hotmail.com>
2021-01-31 10:37:54 +0800

    lesson 15

--- program2/python_0073_data_structure_list_slice.py
+++ program2/python_0073_data_structure_list_slice.py
@@ -19,47 +19,47 @@ print(L)
 print("1) Basic Example ---------------------------------")
 # L[2] in included. L[7] is excluded.
 # This is the same as range(2,10), which generates number [2,10), 2 is included (closed interval), 10 is excluded (open interval)
-print(L[2:7]) # Please figure out output value here, run it, verify your answer.
+print(L[2:7]) # Please figure out & type the output value here, run it, verify your answer.
 # start: 2
 # stop: 7
 # step: 1
 # L[2:7] doesn't have step, so Python assumes step equals to 1
 # so L[2:7] equals to L[2:7:1]
-print(L[2:7:1]) # Please figure out output value here, run it, verify your answer.
+print(L[2:7:1]) # Please figure out & type the output here, run it, verify your answer.
 
 
 print("2) Slice with Negative Indices ------------------")
-print(L[-7:-2]) # Please figure out output value here, run it, verify your answer.
+print(L[-7:-2]) # Please figure out & type the output here, run it, verify your answer.
 # start: -7
 # stop: -2
 # step: 1
 # L[-7:-2] doesn't have step, so Python assumes step equals to 1
 # so L[-7:-2] equals to L[-7:-2：1]
-print(L[-7:-2:1]) # Please figure out output value here, run it, verify your answer.
+print(L[-7:-2:1]) # Please figure out & type the output here, run it, verify your answer.
 
 
 print("3) Slice with Positive & Negative Indices ------------------")
-print(L[2:-5]) # Please figure out output value here, run it, verify your answer.
+print(L[2:-5]) # Please figure out & type the output here, run it, verify your answer.
 
 print("4) Specify Step of the Slicing ------------------------------")
-print(L[2:7:2]) # Please figure out output value here, run it, verify your answer.
+print(L[2:7:2]) # Please figure out & type the output here, run it, verify your answer.
 
 print("5) Negative Step Size ---------------------------------------")
-print(L[6:1:-2]) # Please figure out output value here, run it, verify your answer.
+print(L[6:1:-2]) # Please figure out & type the output here, run it, verify your answer.
 
 print("6) Slice at Beginning & End ---------------------------------")
-print(L[:3]) # Please figure out output value here, run it, verify your answer.
+print(L[:3]) # Please figure out & type the output here, run it, verify your answer.
 #L[:3] doesn't have step, so Python assumes step equals to 1
 # When step equals to 1, when start is missing, Python assumes slice start from the left beginning.
 #L[:3] equals to L[0:3]
 
-print(L[6:]) # Please figure out output value here, run it, verify your answer.
+print(L[6:]) # Please figure out & type the output here, run it, verify your answer.
 #L[6:] doesn't have step, so Python assumes step equals to 1
 # When step equals to 1, when stop is missing, Python assumes slice end till the right end.
 #L[6:] equals to L[6:len(L)]
 
 print("7) Reverse a list ---------------------------------")
-print(L[::-1]) # Please figure out output value here, run it, verify your answer.
+print(L[::-1]) # Please figure out & type the output here, run it, verify your answer.
 # start is missing, stop is missing, step = -1
 # In this case, Python assumes start equals to the right end, stop equals to the left beginning.
 
@@ -70,11 +70,11 @@ L = list("abcde")
 #                   ['a', 'b', 'c', 'd', 'e']
 # negative index:    -5   -4   -3   -2   -1
 L[1:4] = [1,2,3]
-print(L) # Please figure out output value here, run it, verify your answer.
+print(L) # Please figure out & type the output here, run it, verify your answer.
 
 L = list("abcde")
 L[1:2] = [1,2,3]
-print(L) # Please figure out output value here, run it, verify your answer.
+print(L) # Please figure out & type the output here, run it, verify your answer.
 
 print("9) Insert Multiple List Items ------------------------")
 L = list("abc")
@@ -88,7 +88,7 @@ L[:0] = [1,2,3]
 # When step = 1, start is missing, Python assumes slice start from the left beginning.
 # L[:0] equals to L[0:0]
 # When start equals to stop, it points to a POSITION on the left of the index
-print(L) # Please figure out output value here, run it, verify your answer.
+print(L) # Please figure out & type the output here, run it, verify your answer.
 
 L = list("abc")
 L[len(L):] = [1,2,3]
@@ -97,5 +97,5 @@ L[len(L):] = [1,2,3]
 #L[len(L):] equals ot L[len(L):len(L)]
 # When start equals to stop, it points to a position on the left of the index.
 # So L[len(L):len(L)] points to the right of 'c'
-print(L) # Please figure out output value here, run it, verify your answer.
+print(L) # Please figure out & type the output here, run it, verify your answer.
 
