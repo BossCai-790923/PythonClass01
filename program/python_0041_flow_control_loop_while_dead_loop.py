6ae01cfbc1b58037804c44950327459da0185ba7
Tom Fan <tomboxfan@hotmail.com>
2021-02-02 14:31:12 +0800

    remove program

--- program/python_0041_flow_control_loop_while_dead_loop.py
+++
@@ -1,6 +0,0 @@
-
-# This program will run forever, next exit.
-# Because the boolean expression after while is always True
-# We call it dead loop, as soon as python falls into this loop, python is dead. Looping forever, never comes out.
-while True:
-    print('I love Python')
