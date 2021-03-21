77ddf47c3314c51984a56db8ffaf56541a20d124
Tom Fan <tomboxfan@hotmail.com>
2021-03-21 11:42:42 +0800

    Lesson 21

--- program2/python_0103_practice_function_ten_little_indians.py
+++ program2/python_0103_practice_function_ten_little_indians.py
@@ -11,4 +11,11 @@ Ten little, nine little, eight little Indians
 Seven little, six little, five little Indians
 Four little, three little, two little Indians
 One little Indian boy.
+
+In the above 8 lines, 6 of them are quite similar, 2 of them are similar.
+
+The name of the 2 functions are:
+lyric_main(<You_define_parameter_here>)
+lyric_end(<You_define_parameter_here>)
+HINT: You can define multiple parameters inside the parentheses, separated by ','
 '''
