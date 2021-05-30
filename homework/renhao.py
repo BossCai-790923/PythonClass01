b27970fa5dabe36cae8968c9f0a3c1e2f41f9541
Tom Fan <tomboxfan@hotmail.com>
2021-05-30 10:04:51 +0800

    lesson 31

--- homework/renhao.py
+++ homework/renhao.py
@@ -1,7 +1,18 @@
-base = int(input('input base number:'))
-count = int(input('input entry count:'))
-result = base
-for i in range(count - 1):
-    result = result + (result * 10)
+prime_numbers=[]
+for number in range(2, 1001):
+    for i in range(2, number):
+        if number % i == 0:
+            break
+    else:
+        prime_numbers.append(number)
 
-print(result)
+print(prime_numbers)
+
+
+for prime in prime_numbers:
+    a = 2**(prime-1)
+    for i in range(2, a):
+        if a % i == 0:
+            break
+        if i == a-1 and a % a-1 != 0:
+            print(a*((2**prime) - 1))
