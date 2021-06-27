30822c6f4e0cb693e39d90cc9707ec8a0ef0c9ca
Tom Fan <tomboxfan@hotmail.com>
2021-06-27 10:10:38 +0800

    lesson 35

---
+++ program3/python_0170_practice_dict_door_number_tile_count_Q2.py
@@ -0,0 +1,9 @@
+'''
+Now you have 1000 tiles each from '0' to '9'.
+If you start installing door number for '0001'.
+Question: What's the maximum door number you can install till your run out of tiles?
+
+
+A more challenging question:
+Can you use defaultdict in this Question?
+'''
