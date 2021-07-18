72eb5fbb173f622a4ab5942579a57a487b4daca7
Tom Fan <tomboxfan@hotmail.com>
2021-07-18 10:12:21 +0800

    lesson 38

--- program4/python_0197_practice_mrt_transit_station_solution2.py
+++ program4/python_0197_practice_mrt_transit_station_solution2.py
@@ -1,11 +1,7 @@
-'''
-Introduction:
-We are going to design a Singapore MRT transit app.
-Right now, Singapore MRT system has green_line / red_line / yellow_line / purple_line / brown_line, 5 lines.
-Green_line and yellow_line also have sub lines, meaning in total, we have 7 lines.
 
-I've defined the 7 tuples as below:
-'''
+from collections import defaultdict
+from pprint import pprint
+
 
 # PREPARE DATA BEGIN =================================
 
@@ -25,3 +21,44 @@ brown_line = ('Woodlands North', 'Woodlands', 'Woodlands South', 'Springleaf', '
 Requirement: Your task is find out all the transit stations among all the lines.
 '''
 
+
+# 1) I want to give a number to each of the 7 lines
+# The name and line are 1 to 1 mapping, so I use a dictionary to define it.
+line_name_dict = {  'green_line'        : green_line,
+                    'green_sub_line'    : green_sub_line,
+                    'red_line'          : red_line,
+                    'yellow_line'       : yellow_line,
+                    'yellow_sub_line'   : yellow_sub_line,
+                    'purple_line'       : purple_line,
+                    'brown_line'        : brown_line}
+
+station_to_lines_dict = defaultdict(set)
+
+'''
+--------------------------
+Solution 2 Logic
+--------------------------
+I can use a similar data structure as in 196. But I use a more efficient way to find transit stations.
+I can loop all stations of all lines, I handle those stations one by one.
+I add the line name to the set which belongs to the station in the station_to_lines_dict 
+So that I can avoid all those line combinations / set intersection code
+
+I need to refactor the variable name to station_lines_dict, as it will include all stations.
+'''
+
+def populate_station_to_lines_dict():
+    for line_name, line in line_name_dict.items():
+        for station in line:
+            station_to_lines_dict[station].add(line_name)
+
+
+# MAIN PROGRAM BEGIN ==================================
+
+# Step 1) Call the function
+populate_station_to_lines_dict()
+
+# pprint(station_to_lines_dict)
+
+for station, lines in station_to_lines_dict.items():
+    if len(lines) > 1:
+        print(f"{station} is a transit station, which joins {lines}")
