2325ab4efd5eae10c2f1e1759eb2d51110bff0b4
Tom Fan <tomboxfan@hotmail.com>
2021-09-02 22:32:46 +0800

    update

--- homework/bosi.py
+++ homework/bosi.py
@@ -1,91 +1,25 @@
-from python_0210_practice_mrt_data_basic import line_name_dict
-from python_0213_practice_mrt_data_line_to_transit_stations import populate_transit_station_to_lines_dict
-from pprint import pprint
-from collections import defaultdict
-from itertools import permutations
-from itertools import combinations
-# line_name_dict = {  'green_line'        : green_line,
-#                     'green_sub_line'    : green_sub_line,
-#                     'red_line'          : red_line,
-#                     'yellow_line'       : yellow_line,
-#                     'yellow_sub_line'   : yellow_sub_line,
-#                     'purple_line'       : purple_line,
-#                     'brown_line'        : brown_line,
-#                     'blue_line'         : blue_line}
-transit_station_lines_dict = defaultdict(list)
-#Max twice
-change_line_possibilities_dict=defaultdict(list)
-line_to_transit_stations_dict = defaultdict(set)
-def populate_line_to_transit_station_dict():
-    line_combination_set = set(combinations(line_name_dict, 2))
-
-    for line1_n, line2_n in line_combination_set:
-        line1_stations_set = set(line_name_dict[line1_n])
-        line2_stations_set = set(line_name_dict[line2_n])
-        transit_stations = tuple(line1_stations_set & line2_stations_set)
-
-        if len(transit_stations) > 0:  #
-            for transit_station in transit_stations:
-                transit_station_lines_dict[line1_n].append(transit_station)
-                transit_station_lines_dict[line2_n].append(transit_station)
-populate_line_to_transit_station_dict()
-pprint(transit_station_lines_dict)
-
-st = input('start')
-st_line = input('startline')
-end = input('end:')
-endline = input('endline:')
-
-def populate_change_lines_possibilities_dict():
-    line_name_list = list(line_name_dict.keys()) * 2
-    pprint(line_name_list)
-
-    possible_set0 = tuple(permutations(line_name_list, 2))
-    possible_set1 = tuple(permutations(line_name_list, 3))
-    possible_set=possible_set1+possible_set0
-    # pprint(possible_set)
-    for i in range(len(possible_set)):
-        if possible_set[i][0]==st_line and possible_set[i][-1]==endline:
-            if len(possible_set[i]) ==2:
-                for x in range(len(line_name_dict[possible_set[i][0]])):
-                    for y in range(len(line_name_dict[possible_set[i][1]])):
-
-                        if line_name_dict[possible_set[i][0]][x]==line_name_dict[possible_set[i][0]][x]:
-                            ct=1
-                            print(possible_set[i])
-
-                        else:
-                            if len(possible_set[i]) ==3:
-                                for x in range(len(line_name_dict[possible_set[i][0]])):
-                                    for y in range(len(line_name_dict[possible_set[i][1]])):
-
-                                        if line_name_dict[possible_set[i][0]][x] == line_name_dict[possible_set[i][0]][
-                                            x]:
-                                            ct = 1
-                                for x in range(len(line_name_dict[possible_set[i][1]])):
-                                    for y in range(len(line_name_dict[possible_set[i][2]])):
-
-                                        if line_name_dict[possible_set[i][0]][x] ==line_name_dict[possible_set[i][0]][x]:
-                                            ct = 1
-                                            print(possible_set[i])
-                                for x in range(len(line_name_dict[possible_set[i][0]])):
-                                    for y in range(len(line_name_dict[possible_set[i][2]])):
-
-                                        if line_name_dict[possible_set[i][0]][x] ==line_name_dict[possible_set[i][0]][x]:
-                                            ct = 1
+cups=['ball','none','none']
+def A_turn(list):
+    list.insert(0,list.pop(1))
+    return list
+def B_turn(list):
+    list.insert(1,list.pop(2))
+    return list
+def C_turn(list):
+    list.reverse()
+    return list
 
 
 
+if __name__=="__main__":
+    input1=input('please input:')
+    inputlist=list(input1)
+    for i in inputlist:
+        if i == 'a':
+            A_turn(cups)
+        if i == 'b':
+            B_turn(cups)
+        if i == 'c':
+            C_turn(cups)
 
-
-
-
-
-
-
-
-
-populate_transit_station_to_lines_dict()
-
-
-populate_change_lines_possibilities_dict()
+    print(cups.index('ball')+1)
--- homework/zirui.py
+++ homework/zirui.py
@@ -1,59 +1,16 @@
-from math import sqrt
-
-# CLASS DEFINITION BEGIN ==========================
-
-
-class Point:
-
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        print(f"New point ({x, y}) is created.")
-
-    def move_to(self, x, y):
-        self.bx = self.x
-        self.by = self.y
-        self.x = x
-        self.y = y
-        print(f"Moving point ({self.bx, self.by}) to ({x, y}).")
-
-    def move_by(self, dx, dy):
-        print(f"Moving point ({self.x, self.y}) by ({dx, dy}).")
-        self.x += dx
-        self.y += dy
-        print(f"Current location: ({self.x, self.y})")
-
-    def distance_to(self, other):
-        print(sqrt((self.x-other.x)**2 + (self.y-other.y)**2))
-
-    def to_tuple(self):
-        return (self.x, self.y)
-
-    def __str__(self):
-        return f"({self.x}, {self.y})"
-
-
-# MAIN PROGRAM BEGIN =================================
-
-if __name__ == '__main__':
-
-    print("Creating p1 -------------------------")
-    p1 = Point(-10, -10)
-
-    print("Moving p1 -------------------------")
-    p1.move_to(3, 5)
-
-    print("p1 location -------------------------")
-    print(f"p1 location: {p1}")
-
-    print("Creating p2 -------------------------")
-    p2 = Point(0, 0)
-
-    print("Moving p2 by-------------------------")
-    p2.move_by(-1, 2)
-
-    print("p2 location -------------------------")
-    print(f"p2 location: {p2}")
-
-    print("Calculating p1 to p2 distance -------")
-    print(p1.distance_to(p2))
+index = [1, 0, 0]
+swaps = input()
+swaps = list(swaps)
+for i in swaps:
+    if i == 'A':
+        swapped = index.pop(0)
+        index.insert(1, swapped)
+    if i == 'B':
+        swapped = index.pop(1)
+        index.insert(2, swapped)
+    if i == 'C':
+        swapped = index.pop(2)
+        index.insert(0, swapped)
+        swapped = index.pop(1)
+        index.insert(2, swapped)
+print(index.index(1)+1)
