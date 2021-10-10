1cecbbeb1a3ac457ca9ab81022c7a975941a9810
Tom Fan <tomboxfan@hotmail.com>
2021-10-10 10:43:11 +0800

    lesson 49

---
+++ program4/python_0263_algo_recursion_reverse_str.py
@@ -0,0 +1,16 @@
+
+def reverse_str(str1):
+
+    # base case:
+    if len(str1) == 1:
+        return str1
+
+    # recursive call:
+    # homework
+
+
+
+
+
+reversed_str = reverse_str('Hello!')
+print(reversed_str)
---
+++ program4/python_0266_algo_recursion_count_down.py
@@ -0,0 +1,19 @@
+import time
+
+
+
+
+def count_down(n):
+
+    time.sleep(1)
+    print(n)
+
+    # base case
+    if n == 0:
+        return
+
+    # recursive call
+    # HOMEWORK
+
+
+count_down(10)
