0fd6535513a08905344c6f00d2c25260d5fe27cb
Tom Fan <tomboxfan@hotmail.com>
2021-11-07 10:48:43 +0800

    lesson 53

---
+++ program5/python_0311_practice_fibonacci_rabbit.py
@@ -0,0 +1,8 @@
+'''
+HOMEWORK:
+
+In month 1, there is a pair of baby rabbits.
+In month 4, the pair is able to breed another pair.
+
+Assume, the rabbit never dies, calculate the total pairs of the rabbit at month 20.
+'''
